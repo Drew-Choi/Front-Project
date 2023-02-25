c3d5d2bfdcc97478e05d8d05ee1b0b82528ba4fc
Drew <drc920630@gmail.com>
2023-02-26 00:03:27 +0900

    인트로 마지막 제작 중

--- intro/static/introJS.js
+++ intro/static/introJS.js
@@ -1,6 +1,16 @@
-let cloneLine = document.createElement("div");
+const divLine = document.querySelector(".lineEffect");
+const containerEl = document.querySelector(".container");
+const textEl = document.querySelectorAll("text")
 
 for (let i = 0; i < 10; i += 1) {
-cloneLine.classList.add("lineEffect");
-document.querySelector(".container").append(cloneLine);
+  let cloneLine = divLine.cloneNode(false);
+  containerEl.prepend(cloneLine);
+}
+
+const individualLineEl = containerEl.childNodes;
+
+console.log(individualLineEl);
+for (let x = 0; x < 10; x += 1) {
+  textEl.removeChild(textEl);
+  individualLineEl[x].setAttribute("style", `left: ${x * 5}px;`);
 }
