3ce57f3547535be253bb4da4b33d7bf5623bb518
Drew <drc920630@gmail.com>
2023-02-28 22:33:30 +0900

    서칭 아이콘 a태그 #비활성화 완료

--- header/static/headerJS.js
+++ header/static/headerJS.js
@@ -1,18 +1,20 @@
-$(".search__icon").click(() => {
-  if($(".search").hasClass("on") !== true) {
-    $(".search").addClass("on");
-    $(".search__icon").addClass("on");
-    $(".search").attr("placeholder", "노래제목,  가수,  장르");
-  }
-})
-
-$(window).mousedown(function(e) {
-  let eTargetId = e.target.id
-  if(eTargetId !== "no-hidden") {
-    $(".search").removeClass("on");
-    $(".search__icon").removeClass("on");
-    $(".search").removeAttr("placeholder", "노래제목,  가수,  장르");
-  }
+$(document).ready(function() {
+  $(".search__icon").click(() => {
+    if($(".search").hasClass("on") !== true) {
+      $(".search").addClass("on");
+      $(".search__icon").addClass("on");
+      $(".search").attr("placeholder", "노래제목,  가수,  장르");
+    }
+  })
+  
+  $(window).mousedown(function(e) {
+    let eTargetId = e.target.id
+    if(eTargetId !== "no-hidden") {
+      $(".search").removeClass("on");
+      $(".search__icon").removeClass("on");
+      $(".search").removeAttr("placeholder", "노래제목,  가수,  장르");
+    }
+  })
 })
 
 for (let i = 1; i <= 4; i += 1) {
