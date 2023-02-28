33548f1f172a9cb37664cd007951b5f44cd2b606
Drew <drc920630@gmail.com>
2023-02-28 22:21:41 +0900

    헤더 완성 백업

--- header/static/headerJS.js
+++ header/static/headerJS.js
@@ -20,6 +20,18 @@ let profileIcons = document.querySelector(`.icon${i}`);
 profileIcons.setAttribute("style", `background-image: url("../img/netflix_logo/nav_logo/netflixMusic_default-login${i}.png")`);
 }
 
-// $(".login").mouseover(() => {
-//   $(".login__submenus")
-// })
+// 헤더 스크롤이벤트
+//스크롤을 할 때마다 로그로 현재 스크롤의 위치가 찍혀나온다.
+// window.addEventListener('scroll', () => { 
+//   console.log(window.scrollX, window.scrollY);
+// });
+//본코드
+
+window.addEventListener('scroll', () => {
+const innerEl = document.querySelector("header > .inner");
+if(window.scrollY > 50) {
+  innerEl.setAttribute("style", "background-color: black;");
+} else {
+  innerEl.removeAttribute("style");
+}
+})
